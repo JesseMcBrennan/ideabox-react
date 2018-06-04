063b29d5fa46cfce35da8df241252a7842168dbe
Jesse McFadden <jessemcbrennan@gmail.com>
2018-06-04 11:26:26 -0600

    Add Idea Inputs

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,23 +1,25 @@
 import React, { Component } from 'react';
 import Card from '../Card/Card.js'
+import Idea from '../Idea/Idea.js'
 import './App.css'
 
 class App extends Component {
   constructor() {
     super();
     this.state = {
-
+      body: '',
+      title: '',
     }
   }
 
-  CreateCard()
-
-
-
-  render() {
+  CreateCard() {
 
   }
-
-}
+  render() {
+    <div>
+    <Idea ideaContainer={/>
+    </div>
+  };
+};
 
 export default App;
--- src/components/Idea/Idea.js
+++ src/components/Idea/Idea.js
@@ -9,9 +9,32 @@ class Idea extends Component {
     }
   }
 
+  updateIdea(event) {
+    this.setState( { Title: event.target.value } {Body: event.target.value});
+  }
+
+  submitIdeaToApp() {
+    this.setState({
+      this.setState({
+        title: ''
+        body: ''
+      })
+    })
+  }
+
   render() {
     return (
-      <div className="ideaContainer"
-      )
-  }
-}
+      <div className="ideaContainer">
+      <input type 'text'
+        placeholder='title' onChange={this.updateIdea}
+      />
+      <input type 'text' placeholder='body' onChange={ this.updateTitle } 
+      />
+      <button className='submitButton' onClick='submitIdeaToApp'>Submit</button>
+      </div>
+      );
+  };
+};
+
+
+export default Idea;
