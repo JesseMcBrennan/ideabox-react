1410c6ecd69340ecbfd1deb0f63aa70aade526d0
Jesse McFadden <jessemcbrennan@gmail.com>
2018-06-04 11:07:12 -0600

    Add basic file structure

---
+++ src/components/App/App.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+import Card from '../Card/Card.js'
+import './App.css'
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+
+    }
+  }
+
+  CreateCard()
+
+
+
+  render() {
+
+  }
+
+}
+
+export default App;
---
+++ src/components/Idea/Idea.js
@@ -0,0 +1,17 @@
+import React from 'react;'
+
+class Idea extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      Title: ''
+      Body: ''
+    }
+  }
+
+  render() {
+    return (
+      <div className="ideaContainer"
+      )
+  }
+}
